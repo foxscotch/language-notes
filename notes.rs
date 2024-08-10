dc3a0f8435af69322fb6e974cbb3a5897cc85135
Foxscotch <foxscotch@gmail.com>
2024-08-10 00:56:33 -0500

    Add slightly more Rust notes

--- notes.rs
+++ notes.rs
@@ -53,6 +53,47 @@ let unsigned_int64 = 95u64;
 
 // for floats, just f32 and f64, the latter of which is the default
 
+// bool, predictable, true or false
+
+// char on the other hand is special. char isa unicode scalar value. apparently
+// (always?) four bytes. more later on char/string types in general
+
+// all primitive types are defined in the standard library:
+// https://doc.rust-lang.org/std/index.html
+
+//#endregion
+
+
+////-- TUPLES --////
+//#region
+
+// tuples, like in other languages, are sequential collections of values that
+// may be of different types. the tuple itself, however, has a single type: a
+// tuple type comprised of the types of each value
+
+let tup: (i32, f64, u8) = (200, 6.9, 4);
+
+// a tuple's type can be inferred:
+let tup = (200, 2.9, 4);  // of type (i32, f64, i32)
+// and you can use pattern matching to extract individual values
+let (a, b, c) = tup;
+// or access elements by index
+let two_hundred = tup.0;  // == 200
+// mutables tuples can be modified in the way you'd expect
+tup.1 = 5.8;  // tup == (200, 5.8, 4)
+
+// a tuple with no elements is a "unit". it is both a value and a type, in both
+// cases written as `()`. expressions that don't return another value implicitly
+// return the unit value. the unit type is the equivalent of void in C
+
+//#endregion
+
+
+////-- ARRAYS --////
+//#region
+
+// arrays are like C arrays; all one type, fixed-length
+
 //#endregion
 
 
@@ -69,8 +110,18 @@ let unsigned_int64 = 95u64;
 
 // when integer overflows happen in a dev build it causes a PANIC, crashing the
 // whole programs. in release builds, it just overflows like it would in C. but
-// this is bad to rely on; instead, there is a series of math functions for
+// this is bad to rely on; instead, there is a collection of math methods for
 // various behaviors desired upon overflow
-wrapping_add()
+wrapping_add()     // wraps around to the other end of the range
+checked_div()      // returns None on overflow
+overflowing_mul()  // wraps + returns a bool indicating whether it overflowed
+saturating_sub()   // returns value at either end of range rather than overflow
+// even more integer methods can be found here:
+// https://doc.rust-lang.org/std/primitive.u32.html
 
+//#endregion
+
+
+////-- TEMPLATE --////
+//#region
 //#endregion
